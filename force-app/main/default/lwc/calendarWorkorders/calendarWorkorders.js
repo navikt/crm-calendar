1bd610270b0b5a24aaa7cf7d6a823c3a6375e33f
TonjeKaridotterOkland <55350586+TonjeKaridotterOkland@users.noreply.github.com>
2022-02-14 15:19:34 +0100

    Opprettet controller for workorders
    Får ikke hentet ut result.data.

--- force-app/main/default/lwc/calendarWorkorders/calendarWorkorders.js
+++ force-app/main/default/lwc/calendarWorkorders/calendarWorkorders.js
@@ -1,5 +1,26 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track, wire } from 'lwc';
+import getWorkOrder from '@salesforce/apex/CalendarWorkOrdersController.getWorkOrder';
 
 export default class CalendarWorkorders extends LightningElement {
-    test = 'test1';
+    //henter ut workOrder
+    @track workOrder;
+    @wire(getWorkOrder)
+    wiredGetWorkOrder(result) {
+        console.log('test for å se om det er result222222');
+        if (result.data) {
+            this.workOrder = result.data;
+            console.log('test for å se om det er this.workorder.subject' + this.workOrder.subject);
+            console.log('test for å se om det er result.data' + result.data);
+            this.showAllWorkOrders();
+            console.log('test for å se om det er result');
+        }
+        console.log('test for å se om det er tomt');
+    }
+    showAllWorkOrders() {
+        for (var i = 0; i < this.workOrder.length; i++) {
+            console.log('test');
+            console.log(this.workOrder[i].subject);
+            console.log('test');
+        }
+    }
 }
