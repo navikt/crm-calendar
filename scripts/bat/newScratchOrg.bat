12c76920a58fe8fa437edf82e7db340b6cf2cd1e
TonjeKaridotterOkland <55350586+TonjeKaridotterOkland@users.noreply.github.com>
2022-02-14 12:58:04 +0100

    added testdata

--- scripts/bat/newScratchOrg.bat
+++ scripts/bat/newScratchOrg.bat
@@ -7,4 +7,7 @@ call sfdx force:source:push
 echo "Tildeler tilatelsessett til brukeren"
 call sfdx force:user:permset:assign --permsetname CustomCalendar
 
+echo "Oppretter testdata"
+call sfdx force:apex:execute -f scripts/apex/createTestData.apex
+
 echo "Ferdig"
