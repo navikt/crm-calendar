10a665c9d73c66271c0af61142d78682325af054
TonjeKaridotterOkland <55350586+TonjeKaridotterOkland@users.noreply.github.com>
2022-02-15 10:10:51 +0100

    WorkOrders hentet ut og skrevet ut på siden.

--- force-app/main/default/lwc/calendarWorkorders/calendarWorkorders.js
+++ force-app/main/default/lwc/calendarWorkorders/calendarWorkorders.js
@@ -1,26 +1,12 @@
 import { LightningElement, track, wire } from 'lwc';
-import getWorkOrder from '@salesforce/apex/CalendarWorkOrdersController.getWorkOrder';
+import getAllWorkOrders from '@salesforce/apex/CalendarWorkOrdersController.getAllWorkOrders';
 
 export default class CalendarWorkorders extends LightningElement {
-    //henter ut workOrder
-    @track workOrder;
-    @wire(getWorkOrder)
-    wiredGetWorkOrder(result) {
-        console.log('test for å se om det er result222222');
+    @track workOrders;
+    @wire(getAllWorkOrders)
+    wiredgetAllWorkOrders(result) {
         if (result.data) {
-            this.workOrder = result.data;
-            console.log('test for å se om det er this.workorder.subject' + this.workOrder.subject);
-            console.log('test for å se om det er result.data' + result.data);
-            this.showAllWorkOrders();
-            console.log('test for å se om det er result');
-        }
-        console.log('test for å se om det er tomt');
-    }
-    showAllWorkOrders() {
-        for (var i = 0; i < this.workOrder.length; i++) {
-            console.log('test');
-            console.log(this.workOrder[i].subject);
-            console.log('test');
+            this.workOrders = result.data;
         }
     }
 }
