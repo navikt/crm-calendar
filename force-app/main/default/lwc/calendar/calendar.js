cfd8268ba2e7fbb10bf692b4d44a5b203afec3c5
haugland97 <petter.haugland@nav.no>
2022-02-15 10:47:08 +0100

    Initial funciton test

--- force-app/main/default/lwc/calendar/calendar.js
+++ force-app/main/default/lwc/calendar/calendar.js
@@ -2,19 +2,6 @@ import { LightningElement, wire } from 'lwc';
 import { getRecord } from 'lightning/uiRecordApi';
 import USER_ID from '@salesforce/user/Id';
 import NAME_FIELD from '@salesforce/schema/User.FirstName';
+import { getTermOptions } from './myFunction';
 
-export default class Calendar extends LightningElement {
-    helloWorld = 'Hello World';
-    userName;
-    userId;
-    @wire(getRecord, {
-        recordId: USER_ID,
-        fields: [NAME_FIELD]
-    })
-    wireuser({ data }) {
-        if (data) {
-            this.userName = data.fields.FirstName.value;
-            this.userId = data.id;
-        }
-    }
-}
+export default class Calendar extends LightningElement {}
---
+++ force-app/main/default/lwc/calendar/myFunction.js
@@ -0,0 +1,8 @@
+const getTermOptions = () => {
+    return [
+        { label: '20 years', value: 20 },
+        { label: '25 years', value: 25 }
+    ];
+};
+
+export { getTermOptions };
