ddaeeeff6465197683e8f469722150ef51927df1
JanRobertNav <98319833+JanRobertNav@users.noreply.github.com>
2022-03-01 10:36:11 +0100

    Added service appointments
    Added instructions on how to delete scratchOrg in 'newScratchOrgMac'.
    Added function for getting ServiceAppointments (Not being tested)
    Added javascript and html to display Service Appointments.

--- force-app/main/default/lwc/calendarWorkorders/calendarWorkorders.js
+++ force-app/main/default/lwc/calendarWorkorders/calendarWorkorders.js
@@ -1,6 +1,7 @@
 import { LightningElement, track, wire } from 'lwc';
 import getAllWorkOrders from '@salesforce/apex/CalendarWorkOrdersController.getAllWorkOrders';
 import getAllAccounts from '@salesforce/apex/CalendarWorkOrdersController.getAllAccounts';
+import getAllServiceAppointments from '@salesforce/apex/CalendarWorkOrdersController.getAllServiceAppointments';
 
 export default class CalendarWorkorders extends LightningElement {
     @track workOrders;
@@ -16,6 +17,14 @@ export default class CalendarWorkorders extends LightningElement {
     wiredgetAllAccounts(result) {
         if (result.data) {
             this.allAccounts = result.data;
+        }
+    }
+
+    @track allServiceAppointments;
+    @wire(getAllServiceAppointments)
+    wiredgetAllServiceAppointments(result) {
+        if (result.data) {
+            this.allServiceAppointments = result.data;
         }
     }
 }
