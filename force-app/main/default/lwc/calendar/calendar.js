b1a9785993b2a03ca4fec3eff535c091bffd55fe
haugland97 <98319821+haugland97@users.noreply.github.com>
2022-03-09 10:33:13 +0100

    Calendar frame (#8)
    * Created calendarFrame
    
    * Revert "Created calendarFrame"
    
    This reverts commit 2b6caf4e6890b7c1993fdc13cb6d46197317a9a3.
    
    * Revert "Created calendarFrame"
    
    This reverts commit 2b6caf4e6890b7c1993fdc13cb6d46197317a9a3.
    
    * Update .gitignore
    
    * added testdata
    
    * css only
    
    * Initial funciton test
    
    * test-gitIgnore
    
    * first calendar commit
    
    * extracted calendarBody to a :100:
    
    * Update sfdx-config.json
    
    * im tired
    
    * Add swapping of months
    
    * css
    
    * calendar changes and css
    
    Co-authored-by: MagnusNav <magnus.christian.moller.jensen@nav.no>
    Co-authored-by: TonjeKaridotterOkland <55350586+TonjeKaridotterOkland@users.noreply.github.com>

--- force-app/main/default/lwc/calendar/calendar.js
+++ force-app/main/default/lwc/calendar/calendar.js
@@ -1,20 +1,9 @@
-import { LightningElement, wire } from 'lwc';
+import { api, LightningElement, wire } from 'lwc';
 import { getRecord } from 'lightning/uiRecordApi';
 import USER_ID from '@salesforce/user/Id';
 import NAME_FIELD from '@salesforce/schema/User.FirstName';
 
+
 export default class Calendar extends LightningElement {
-    helloWorld = 'Hello World';
-    userName;
-    userId;
-    @wire(getRecord, {
-        recordId: USER_ID,
-        fields: [NAME_FIELD]
-    })
-    wireuser({ data }) {
-        if (data) {
-            this.userName = data.fields.FirstName.value;
-            this.userId = data.id;
-        }
-    }
+
 }
---
+++ force-app/main/default/lwc/calendarBody/calendarBody.js
@@ -0,0 +1,125 @@
+import { LightningElement, track } from 'lwc';
+
+export default class CalendarBody extends LightningElement {
+    weekdays = ['mandag', 'tirsdag', 'onsdag', 'torsdag', 'fredag', 'lørdag', 'søndag'];
+    reversedWeekdays = ['mandag', 'tirsdag', 'onsdag', 'torsdag', 'fredag', 'lørdag', 'søndag'].reverse();
+    calendarContainer = document.getElementsByClassName('calendar-container');
+    monthNav = 0;
+    currentMonth = 'Test';
+
+    handlePrev() {
+        this.monthNav--;
+        this.load(this.template.querySelector('div'));
+    }
+    handleNext() {
+        this.monthNav++;
+        this.load(this.template.querySelector('div'));
+    }
+
+    renderedCallback() {
+        this.load(this.template.querySelector('div'));
+    }
+
+    load(element) {
+        const dt = new Date();
+
+        if (this.monthNav !== 0) {
+            dt.setMonth(new Date().getMonth() + this.monthNav);
+        }
+
+        const day = dt.getDate();
+        const month = dt.getMonth();
+        const year = dt.getFullYear();
+
+        const firstDayOfMonth = new Date(year, month, 1);
+        const lastDayOfMonth = new Date(year, month + 1, 0);
+        console.log('Last day: ', lastDayOfMonth);
+        const daysInMonth = new Date(year, month + 1, 0).getDate();
+
+        const firstDateString = firstDayOfMonth.toLocaleDateString('no', {
+            weekday: 'long',
+            year: 'numeric',
+            month: 'numeric',
+            day: 'numeric'
+        });
+        console.log(firstDateString);
+
+        const lastDateString = lastDayOfMonth.toLocaleDateString('no', {
+            weekday: 'long',
+            year: 'numeric',
+            month: 'numeric',
+            day: 'numeric'
+        });
+
+        const paddingDaysFirst = this.weekdays.indexOf(firstDateString.split(' ')[0]);
+        const paddingDaysLast = this.reversedWeekdays.indexOf(lastDateString.split(' ')[0]);
+        console.log('Padding: ', paddingDaysFirst);
+        console.log('Padding After: ', paddingDaysLast);
+
+        let prevPaddingDays = [];
+        for (let j = 1; j <= paddingDaysFirst; j++) {
+            let d = firstDayOfMonth;
+            d.setDate(d.getDate() - 1);
+            prevPaddingDays.push(d.getDate());
+        }
+        prevPaddingDays.reverse();
+
+        let nextPaddingDays = [];
+        for (let j = 1; j <= paddingDaysLast; j++) {
+            let d = lastDayOfMonth;
+            d.setDate(d.getDate() + 1);
+            nextPaddingDays.push(d.getDate());
+        }
+
+        this.currentMonth = `${dt.toLocaleDateString('no', { month: 'long' })} ${year}`;
+
+        element.innerHTML = `
+        <div class="slds-col slds-size_1-of-7 day-header">
+            <span> Man </span>
+        </div>
+        <div class="slds-col slds-size_1-of-7 day-header">
+            <span> Tir </span>
+        </div>
+        <div class="slds-col slds-size_1-of-7 day-header">
+            <span> Ons </span>
+        </div>
+        <div class="slds-col slds-size_1-of-7 day-header">
+            <span> Tor </span>
+        </div>
+        <div class="slds-col slds-size_1-of-7 day-header">
+            <span> Fre </span>
+        </div>
+        <div class="slds-col slds-size_1-of-7 day-header">
+            <span> Lør </span>
+        </div>
+        <div class="slds-col slds-size_1-of-7 day-header">
+            <span> Søn </span>
+        </div>`;
+
+        const test = 1;
+        for (let i = 1; i <= paddingDaysFirst + daysInMonth + paddingDaysLast; i++) {
+            const daySquare = document.createElement('div');
+            daySquare.classList.add('day');
+            daySquare.classList.add('slds-col');
+            daySquare.classList.add('slds-size_1-of-7');
+            daySquare.style.float = 'right';
+            daySquare.style.border = '0.5px solid rgb(154, 154, 154)';
+            daySquare.style.height = '150px';
+            daySquare.style.paddingLeft = '6px';
+            daySquare.style.paddingTop = '3px';
+            daySquare.style.fontWeight = '500';
+
+            if (i > paddingDaysFirst && i <= daysInMonth + 1) {
+                daySquare.innerText = i - paddingDaysFirst;
+            } else if (i <= paddingDaysFirst) {
+                daySquare.innerText = prevPaddingDays[i - 1];
+                daySquare.style.opacity = '0.5';
+            } else if (i > daysInMonth) {
+                console.log(nextPaddingDays[i]);
+                daySquare.innerText = nextPaddingDays[i - 1];
+            }
+
+            element.appendChild(daySquare);
+        }
+    }
+}
